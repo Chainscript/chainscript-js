ffb6496e5368bbe7575e29547bb298e3e9820a89
Stephan Florquin <stephan.florquin@gmail.com>
2015-09-18 23:41:27 +0200

    Tweak deepEquals

--- src/utils/deepEquals.js
+++ src/utils/deepEquals.js
@@ -1,9 +1,5 @@
 import { digest } from 'json-hash';
 
 export default function deepEquals(a, b) {
-  if (a === b) {
-    return true;
-  }
-
-  return digest(a) === digest(b);
+  return a === b || digest(a) === digest(b);
 }
